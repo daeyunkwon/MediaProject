500e26308f95a11a80f8076da39bba0a6dcd3e6d
Daeyun <sweet_ray@naver.com>
2024-06-10 15:09:50 +0900

    Initial Commit

---
+++ MediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProject
+//
+//  Created by 권대윤 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
