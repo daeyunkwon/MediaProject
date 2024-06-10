35248c18575d56ab7370a4afe4d8c8f211448ed0
Daeyun <sweet_ray@naver.com>
2024-06-10 19:22:54 +0900

    Fix: 서버에서 media 이름과 개봉일에 대해서 값을 받을 수 있도록 프로퍼티 추가 및 데이터 모델 구조 수정

--- MediaProject/Model/Trend.swift
+++ MediaProject/Model/Trend.swift
@@ -12,19 +12,32 @@ struct TrendData: Codable {
 }
 
 struct Trend: Codable {
-    let id: String?
+    let id: Int?
     let posterPath: String?
     let title: String?
+    let name: String?
     let releaseDate: String?
+    let firstAirDate: String?
     let overview: String?
     let type: String?
+    let voteAverage: Float?
+    
+    var voteAverageString: String? {
+        let formatter = NumberFormatter()
+        formatter.roundingMode = .floor
+        formatter.maximumFractionDigits = 1
+        return formatter.string(from: (voteAverage ?? 0) as NSNumber)
+    }
     
     enum CodingKeys: String, CodingKey {
         case id
         case posterPath = "poster_path"
         case title
+        case name
         case releaseDate = "release_date"
+        case firstAirDate = "first_air_date"
         case overview
         case type = "media_type"
+        case voteAverage = "vote_average"
     }
 }
